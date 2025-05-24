194f5aa61f89a3aed825606e7b47840b747b6300
voiculescubogdan <voiculescubogdan22@stud.ase.ro>
2025-05-24 21:33:14 +0300

    arbore binar de cautare (bst)

---
+++ Arbore Binar de Cautare (BST)/main.c
@@ -0,0 +1,217 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+typedef struct Examen Examen;
+typedef struct Nod Nod;
+
+struct Examen {
+	int id;
+	char* materie;
+	float nota;
+};
+
+struct Nod {
+	Examen info;
+	Nod* stanga;
+	Nod* dreapta;
+};
+
+Examen initExamen(int id, char* materie, float nota) {
+	Examen examen;
+	examen.id = id;
+	examen.nota = nota;
+	examen.materie = (char*)malloc(sizeof(char) * (strlen(materie) + 1));
+	strcpy(examen.materie, materie);
+
+	return examen;
+}
+
+void afisareExamen(Examen examen) {
+	printf("\nExamenul la materia %s, cu id-ul %d, are nota de trecere %.2f", examen.materie, examen.id, examen.nota);
+}
+
+// daca nu exista radacina, o creeaza
+// apoi in stanga cu id mai mic, in dreapta cu id mai mare
+void inserareBST(Nod** radacina, Examen examen) {
+	if (*radacina == NULL) {
+		Nod* nou = (Nod*)malloc(sizeof(Nod));
+		nou->info = examen;
+		nou->stanga = NULL;
+		nou->dreapta = NULL;
+		*radacina = nou;
+	}
+	else if (examen.id < (*radacina)->info.id) {
+		inserareBST(&(*radacina)->stanga, examen);
+	}
+	else {
+		inserareBST(&(*radacina)->dreapta, examen);
+	}
+}
+
+// pentru parcurgeri in ordine crescatoare sau descrescatoare (schimba stanga cu dreapta si invers)
+void parcurgereInordineArbore(Nod* radacina) {
+	if (radacina) {
+		parcurgereInordineArbore(radacina->stanga);
+		afisareExamen(radacina->info);
+		parcurgereInordineArbore(radacina->dreapta);
+	}
+}
+
+//viziteaza intai subarborii, apoi radacina
+void parcurgerePostordineArbore(Nod* radacina) {
+	if (radacina) {
+		parcurgerePostordineArbore(radacina->stanga);
+		parcurgerePostordineArbore(radacina->dreapta);
+	}
+}
+
+// daca id == radacina, inseamna ca este chiar radacina
+// daca id < radacina, inseamna ca trebuie cautat in stanga
+// daca id > radacina, inseamna ca trebuie cautat in dreapta
+Examen cautareById(Nod* radacina, int id) {
+	if (radacina == NULL) {
+		return initExamen(-1, "", 0.0);
+	}
+	else if (id == radacina->info.id) {
+		return initExamen(radacina->info.id, radacina->info.materie, radacina->info.nota);
+	}
+	else if (id > radacina->info.id) {
+		return cautareById(radacina->dreapta, id);
+	}
+	else {
+		return cautareById(radacina->stanga, id);
+	}
+}
+
+void stergereDinArbore(Nod** radacina, int id) {
+	if (!radacina || !*radacina) return;
+
+	if (id < (*radacina)->info.id) {
+		stergereDinArbore(&(*radacina)->stanga, id);
+	}
+	else if (id > (*radacina)->info.id) {
+		stergereDinArbore(&(*radacina)->dreapta, id);
+	}
+	// a fost gasit nodul pe care vrem sa il stergem
+	else {
+		// daca este nod frunză
+		if (!(*radacina)->stanga && !(*radacina)->dreapta) {
+			free((*radacina)->info.materie);
+			free(*radacina);
+			*radacina = NULL;
+		}
+		// daca are un singur copil
+		else if (!(*radacina)->stanga || !(*radacina)->dreapta) {
+			Nod* copil = (*radacina)->stanga ? (*radacina)->stanga : (*radacina)->dreapta;
+			free((*radacina)->info.materie);
+			free(*radacina);
+			*radacina = copil;
+		}
+		// daca are doi copii → găsesc succesorul
+		else {
+			// parcurg spre stânga cât e nevoie, pornind din fiul drept
+			Nod* parinte = *radacina;
+			Nod* fiu = (*radacina)->dreapta;
+
+			while (fiu->stanga) {
+				parinte = fiu;
+				fiu = fiu->stanga;
+			}
+
+			// copiez datele fiului în nodul curent
+			free((*radacina)->info.materie);
+			(*radacina)->info.id = fiu->info.id;
+			(*radacina)->info.nota = fiu->info.nota;
+			(*radacina)->info.materie = malloc(strlen(fiu->info.materie) + 1);
+			strcpy((*radacina)->info.materie, fiu->info.materie);
+
+			// apoi elimin fiul din arbore
+			// copilul lui "fiu" poate avea doar subarbore drept
+			Nod* subarb = fiu->dreapta;
+			// legături de părinte
+			if (parinte->stanga == fiu)
+				parinte->stanga = subarb;
+			else
+				parinte->dreapta = subarb;
+
+			// eliberez memoria fiu
+			free(fiu->info.materie);
+			free(fiu);
+		}
+	}
+}
+
+Examen extragereDinArbore(Nod** radacina, int id) {
+	// 1) întâi găsesc examenul
+	Examen examen = cautareById(*radacina, id);
+	// 2) dacă există, îl şterg
+	if (examen.id != -1) {
+		stergereDinArbore(radacina, id);
+	}
+	return examen;
+}
+
+// verifica daca are copil in stanga si in dreapta
+// daca nu are, inseamna ca este frunza
+// daca are, se reia procesul
+void afisareFrunze(Nod* radacina) {
+	if (!radacina) return;
+
+	if (!radacina->stanga && !radacina->dreapta) {
+		afisareExamen(radacina->info);
+	}
+	else {
+		afisareFrunze(radacina->stanga);
+		afisareFrunze(radacina->dreapta);
+	}
+}
+
+void dezalocare(Nod** radacina) {
+	if (*radacina) {
+		dezalocare(&(*radacina)->stanga);
+		dezalocare(&(*radacina)->dreapta);
+		free((*radacina)->info.materie);
+		free(*radacina);
+	}
+}
+
+void main() {
+	Nod* radacina = NULL;
+
+	inserareBST(&radacina, initExamen(7, "SDD", 5));
+	inserareBST(&radacina, initExamen(4, "PAW", 5));
+	inserareBST(&radacina, initExamen(9, "PEAG", 5));
+	inserareBST(&radacina, initExamen(3, "JAVA", 5));
+	inserareBST(&radacina, initExamen(5, "MACRO", 5));
+	inserareBST(&radacina, initExamen(8, "POO", 5));
+	inserareBST(&radacina, initExamen(2, "ATP", 5));
+	inserareBST(&radacina, initExamen(1, "SGBD", 5));
+
+	printf("\n\nParcurgere inordine arbore: ");
+	parcurgereInordineArbore(radacina);
+
+	printf("\n\nParcurgere postordine arbore: ");
+	parcurgerePostordineArbore(radacina);
+
+	printf("\n\nCautare nod cu id 8");
+
+	Examen examen = cautareById(radacina, 8);
+	afisareExamen(examen);
+
+	printf("\n\nAfisare frunze: ");
+	afisareFrunze(radacina);
+
+	printf("\n\nStergere nod: ");
+	stergereDinArbore(&radacina, 7);
+	parcurgerePostordineArbore(radacina);
+
+	printf("\n\nExtragere nod: ");
+	Examen examenExtras;
+	examenExtras = extragereDinArbore(&radacina, 4);
+	afisareExamen(examenExtras);
+
+	dezalocare(&radacina);
+}
